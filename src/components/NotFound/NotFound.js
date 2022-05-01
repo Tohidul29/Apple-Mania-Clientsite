21015fa0ad48fd9ed33d50333e84bdcf0dfce88c
Tohidul29 <tohidswfu@gmail.com>
2022-05-02 03:17:50 +0600

    basic structure, route added and 404 page create

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../../img/Not-Found-img/notfound.jpg'
+
+const NotFound = () => {
+    return (
+        <div className='text-center w-100 mt-5'>
+            <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
