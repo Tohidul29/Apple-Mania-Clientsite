c0f55199e9d5ce526f47370e5401490f4115b0b5
Tohidul29 <tohidswfu@gmail.com>
2022-05-07 03:30:03 +0600

    inventory page code updated

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Button, Table } from 'react-bootstrap';
+import useProducts from '../../hooks/useProducts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash, faArrowAltCircleUp } from '@fortawesome/free-solid-svg-icons';
 
 const Inventory = () => {
+    const [products, setProducts] = useProducts();
     return (
-        <div>
-            <h3>This is inventory page</h3>
+        <div className='w-75 mx-auto mt-4'>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr className='text-center'>
+                        <th>Product Name</th>
+                        <th>Image</th>
+                        <th>Description</th>
+                        <th>Supplier</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                        <th>Delivered</th>
+                    </tr>
+                </thead>
+                <tbody className='text-center'>
+                    {
+                        products.map(product => <tr>
+                            <td>{product.name}</td>
+                            <td className='text-center'><img className='rounded' width={100} height={100} src={product.img} alt="" /></td>
+                            <td>{product.description}</td>
+                            <td>{product.supplier}</td>
+                            <td>${product.price}</td>
+                            <td>{product.quantity} pieces</td>
+                            <td><Button><FontAwesomeIcon icon={faEdit}></FontAwesomeIcon></Button></td>
+                            <td><Button className='btn-danger'><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button></td>
+                            <td><Button className='btn-warning'><FontAwesomeIcon icon={faArrowAltCircleUp}></FontAwesomeIcon></Button></td>
+                        </tr>)
+                    }
+                </tbody>
+            </Table>
         </div>
     );
 };
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -5,10 +5,24 @@ import { faShop } from '@fortawesome/free-solid-svg-icons';
 import Product from './Product/Product';
 import { Button } from 'react-bootstrap';
 import '../Products/Products.css'
+import { useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Products = () => {
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate()
     const [products, setProducts] = useProducts();
     const productSlicing = products.slice(0, 6);
+
+    const handleManageButton = event =>{
+        if(user){
+            navigate('/inventory');
+        }
+        else{
+            navigate('/login');
+        }
+    }
     return (
         <div>
             <h2 className='header-text text-center mt-5'>Stock Products <FontAwesomeIcon icon={faShop}></FontAwesomeIcon></h2>
@@ -22,7 +36,7 @@ const Products = () => {
                 }
             </div>
             <div className='text-center my-5'>
-                <Button className='btn-lg button-style'>Manage All Products</Button>
+                <Button className='btn-lg button-style' onClick={handleManageButton}>Manage All Products</Button>
             </div>
 
         </div>
